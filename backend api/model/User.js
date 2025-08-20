801b5a0450d4fe3c8d9194222fe95d4a838887a6
abe.mariacherlyn <abe.mariacherlyn@gmail.com>
2025-08-20 18:24:44 +0900

    added user register and login

---
+++ backend api/model/User.js
@@ -0,0 +1,27 @@
+const mongoose = require ("mongoose");
+
+const userSchema = new mongoose.Schema({
+	fullName: {
+		type: String,
+		required: [true, "Full Name is required"]
+	},
+
+	email: {
+		type: String,
+		required: [true, "Email is required"],
+		unique: true
+	},
+
+	password: {
+		type: String,
+		required: [true, "Password is required"], 
+		minLength: [8, "Password must be atleast 8 characters long"]
+	},
+
+	isAdmin: {
+		type: Boolean,
+		default: false
+	}
+})
+
+module.exports = mongoose.model("User", userSchema)
